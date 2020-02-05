cf06e4b44496a9ac108c0d2e8a97b7289de559b9
Logan Donoughe <logandonoughe@gmail.com>
2020-02-04 19:15:44 -0800

    Adds linter

--- main.rb
+++ main.rb
@@ -1,43 +1,41 @@
+# frozen_string_literal: true
+
 
 class Game
+  def initialize
+    @state = Hash.new('.')
+    @state[[3, 5]] = 'R'
+  end
 
-    def initialize
-        @state = Hash.new('.')
-        @state[[3,5]] = 'R'
-    end
+  def add_elk
+    @state[[60, 5]] = Elk.new
+  end
 
-    def add_elk
-        @state[[60, 5]] = Elk.new
+  def display
+    a = (0..9).to_a
+    puts a.join
+    (1..9).each do |y|
+      a = [y]
+      (2..60).each do |x|
+        a += [@state[[x, y]].to_s]
+      end
+      puts a.join
     end
-
-    def display
-        a = (0..9).to_a
-        puts a.join()
-        (1..9).each do |y|
-            a = [y]
-            (2..60).each do |x|
-                a += [@state[[x,y]].to_s]
-            end
-            puts a.join()
-        end
-    end
+  end
 end
 
 class Elk
-
-    def to_s
-        'e'
-    end
-
+  def to_s
+    'e'
+  end
 end
-
 
 g = Game.new
 g.display
 
 # puts "Place Towers"
 
-puts "Elk will now attack the ruby"
+puts 'Elk will now attack the ruby'
 
 g.add_elk
 g.display
