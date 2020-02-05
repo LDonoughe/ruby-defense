0b125cf1bac7ca7710da6216859314dd103c4a98
Logan Donoughe <logandonoughe@gmail.com>
2020-02-04 19:11:08 -0800

    Adds in first elk

--- main.rb
+++ main.rb
@@ -2,18 +2,21 @@
 class Game
 
     def initialize
-        @state = [[]*9]
+        @state = Hash.new('.')
+        @state[[3,5]] = 'R'
+    end
+
+    def add_elk
+        @state[[60, 5]] = Elk.new
     end
 
     def display
+        a = (0..9).to_a
+        puts a.join()
         (1..9).each do |y|
             a = [y]
             (2..60).each do |x|
-                if y == 5 && x == 10
-                    a += ['R']
-                else
-                    a += ['.']
-                end
+                a += [@state[[x,y]].to_s]
             end
             puts a.join()
         end
@@ -22,10 +25,19 @@ end
 
 class Elk
 
+    def to_s
+        'e'
+    end
+
 end
 
 
 g = Game.new
 g.display
 
-puts "Place Towers"
+# puts "Place Towers"
+
+puts "Elk will now attack the ruby"
+
+g.add_elk
+g.display
