b80d26401ab2214b378def50b8a487530792fabd
Logan Donoughe <logandonoughe@gmail.com>
2020-02-05 18:06:28 -0800

    Allows elk to move and steal ruby

--- main.rb
+++ main.rb
@@ -1,14 +1,32 @@
 # frozen_string_literal: true
 
-
 class Game
   def initialize
     @state = Hash.new('.')
     @state[[3, 5]] = 'R'
   end
 
+  def attack_phase
+    until @state['elk'].empty?
+      @state['elk'].each do |e|
+        e.update_position(@state, false, false)
+        display
+        if e.x == 61
+          puts 'Elk Have Captured the Ruby. Game Over'
+          return true
+        end
+      end
+    end
+  end
+
   def add_elk
-    @state[[60, 5]] = Elk.new
+    e = Elk.new(60, 5)
+    if @state['elk'].is_a? Array
+      @state['elk'] += [e]
+    else
+      @state['elk'] = [e]
+    end
+    @state[[60, 5]] = e
   end
 
   def display
@@ -25,7 +43,32 @@ class Game
 end
 
 class Elk
+  def initialize(x, y)
+    @x = x
+    @y = y
+    @ruby = false
+  end
+
+  attr_reader :x
+  attr_reader :ruby
+
+  def update_position(state, x, y)
+    x_old = @x
+    y_old = @y
+    @y = y || @y
+    @x = if @ruby
+           x || @x + 1
+         else
+           x || @x - 1
+         end
+    @ruby = true if state[[@x, @y]] == 'R'
+    state[[x_old, y_old]] = '.'
+    state[[@x, @y]] = self
+  end
+
   def to_s
+    return 'ë' if @ruby == true
+
     'e'
   end
 end
@@ -34,8 +77,11 @@ g = Game.new
 g.display
 
 # puts "Place Towers"
+# g.build_phase
 
 puts 'Elk will now attack the ruby'
 
 g.add_elk
 g.display
+
+g.attack_phase
